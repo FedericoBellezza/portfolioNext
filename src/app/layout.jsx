d898aaa7f2127c4f801c33b7436fb5b6a88dbb79
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-04-30 18:23:09 +0200

    fix and deploy

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,7 +20,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-linear-to-tr from-slate-700  to-gray-800 text-slate-800 overflow-x-hidden`}
       >
